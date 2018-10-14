78f5f19c17ad9ce4a1a4cbcb0f2479d4a5c3ed5b
Marek Glowacki <m.glowacki24h@gmail.com>
2018-10-14 18:29:43 +0200

    wszystkie z niedzieli. Dobrze działa gra 2

---
+++ src/pl/coderslab/Zadanie_1/App.java
@@ -0,0 +1,52 @@
+package pl.coderslab.Zadanie_1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+
+        gra1();
+
+    }
+
+    static void gra1() {
+        Random random = new Random();
+
+        int l = random.nextInt(100) + 1;
+
+        System.out.println(" Zabawmy się, ha ha! Ja wymyślam liczbę, a Ty ją zgadujesz, co Ty na to? Podaj jakąś propozycję liczby z zakresu 1-100: ");
+
+
+
+        Scanner scan = new Scanner(System.in);
+        int ml = 0;
+        while (true) {
+            while (!scan.hasNextInt()) {
+                scan.nextLine();
+
+                System.out.println("Musisz podac liczbe calkowita.");
+
+            }
+
+            ml = scan.nextInt();
+            if (ml > 100 || ml < 1) {
+
+                System.out.println("Musisz podac liczbe calkowita z zakresu 1-100.");
+            } else {
+                if (ml<l) {
+                    System.out.println("Za mało!");
+                } else if (ml>l) {
+                    System.out.println("Za dużo!");
+                } else {
+                    System.out.println("Doskonale, jesteś zwycięzcą! \nNa początku pomyślałem sobie o liczbie: " + l + "\n\nDzięki za miło spędzony czas. Dobrze się z Tobą bawiłem :)");
+                    break;
+                }
+
+            }
+        }
+
+    }
+}
